08351a55f4a8d948658d6b10502efb55d845d6af
MaksymZubarev <maxzub.school@gmail.com>
2024-10-30 08:55:08 +0200

    Started work in MainActivity
    Added binding, started work with button functions, changed app:srcCompat to android:src in xml file due to images being PNG

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.diceroller.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnRoll.setOnClickListener {
+            lifecycleScope.launch {
+                rollStart()
+            }
+        }
+
+        binding.btnStop.setOnClickListener {
+            lifecycleScope.launch {
+                rollStop()
+            }
         }
     }
+
+    private suspend fun rollStart() {
+
+        binding.btnRoll.isEnabled = false
+        binding.btnRoll.visibility = View.GONE
+
+        binding.btnStop.isEnabled = true
+        binding.btnStop.visibility = View.VISIBLE
+
+        Toast.makeText(this, "ROLL! clicked", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private suspend fun rollStop() {
+
+        binding.btnStop.isEnabled = false
+        binding.btnStop.visibility = View.GONE
+
+        binding.btnRoll.isEnabled = true
+        binding.btnRoll.visibility = View.VISIBLE
+
+        Toast.makeText(this, "STOP clicked", Toast.LENGTH_SHORT).show()
+
+    }
+
 }
